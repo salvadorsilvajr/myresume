733094832410950e67d4e6ccc029f9664062abdc
salvadorsilvajr <salvadorsilvajr@yahoo.com>
2019-06-02 02:09:31 +0000

    fist commit of my resume

---
+++ app.js
@@ -0,0 +1,71 @@
+var express = require("express"),
+  app = express(),
+  bodyParser = require("body-parser"),
+  serveStatic = require("serve-static"),
+  nodemailer = require("nodemailer");
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+
+// // INDEX ROUTE show all campgrounds !!
+// app.get("/landing", function(req, res) {
+//   // get all campgrouds from DB:
+// });
+
+app.get("/", function(req, res) {
+  res.render("landing");
+});
+
+// app.get("/coment", function(req, res) {
+//   res.render("coment.ejs");
+// });
+
+// app.post("/coment", function(req, res) {
+//   var name = req.body.name;
+//   var company = req.body.company;
+//   var phone = req.body.phone;
+//   var email = req.body.email;
+//   var coment = req.body.coment;
+//   var newcoment = { name: name, phone: phone, enail: email, coment: coment };
+//   console.log(newcoment);
+
+//   let transporter = nodemailer.createTransport({
+//     service: "gmail",
+//     secure: false,
+//     port: 25,
+//     auth: {
+//       user: "ssilvasweb@gmail.com",
+//       pass: "Fried@3306"
+//     },
+//     tls: {
+//       rejectUnauthorarized: false
+//     }
+//   });
+//   let HelperOptions = {
+//     from: '"salvadorform" <ssilvasweb@gmail.com> ',
+//     to: "ssilvasweb@gmail.com",
+//     subject: "test from my form",
+//     text: " A new Coment from app",
+//     html:
+//       "<p><h1> nombre " +
+//       name +
+//       "</h1></P><p><h2>cellphone " +
+//       phone +
+//       "</h2> </p> <p><h3> Comentario" +
+//       coment +
+//       "</h3></P>"
+//   };
+//   transporter.sendMail(HelperOptions, (error, info) => {
+//     if (error) {
+//       return console.log(error);
+//     }
+//     console.log("the message has sended it"), console.log(info);
+//     res.render("coment.ejs");
+//   });
+// });
+
+
+app.listen(3000, process.env.IP, function(){
+   console.log("The Resume Server Has Started!" + this.address().port);
+});
---
+++ public/stylesheets/js/index.js
@@ -0,0 +1,31 @@
+function sele1() {
+  var kids = document.getElementById("pages").children;
+  for (i = 0; i < kids.length; i++) {
+    kids[i].className = "info1";
+  }
+  kids[0].className = "info2";
+}
+
+function sele2() {
+  var kids = document.getElementById("pages").children;
+  for (i = 0; i < kids.length; i++) {
+    kids[i].className = "info1";
+  }
+  kids[1].className = "info2";
+}
+
+function sele3() {
+  var kids = document.getElementById("pages").children;
+  for (i = 0; i < kids.length; i++) {
+    kids[i].className = "info1";
+  }
+  kids[2].className = "info2";
+}
+
+function sele4() {
+  var kids = document.getElementById("pages").children;
+  for (i = 0; i < kids.length; i++) {
+    kids[i].className = "info1";
+  }
+  kids[3].className = "info2";
+}
